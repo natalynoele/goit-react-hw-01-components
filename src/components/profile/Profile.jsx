6212205631b1167e2327d4155ac2c7adbe040501
natalynoele <natalynoele@gmail.com>
2023-03-02 21:44:24 +0200

    add styles file

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,18 +1,17 @@
 import PropTypes from 'prop-types';
-import State from './State';
 import css from './Profile.module.css';
+import State from './State';
 
 const Profile = ({ username, avatar, tag, location, stats }) => {
   const { followers, likes, views } = stats;
   return (
     <div className={css.profile}>
       <div className={css.description}>
-        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <img src={avatar} className={css.avatar} alt="User avatar" />
         <p className={css.name}>{username}</p>
         <p className={css.tag}>@{tag}</p>
         <p className={css.location}>{location}</p>
       </div>
-
       <ul className={css.stats}>
         <State label="Followers" quantity={followers} />
         <State label="Views" quantity={views} />
